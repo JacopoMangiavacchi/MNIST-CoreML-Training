51d86129b000a7bfb9e5e8bebbaf231eba60feb3
Jacopo Mangiavacchi <jamangia@microsoft.com>
2020-03-31 08:48:42 -0700

    async prepare batch provider

--- MNIST-CoreML-Training/ContentView.swift
+++ MNIST-CoreML-Training/ContentView.swift
@@ -1,67 +1,30 @@
 //
-//  MNIST.swift
-//  CoreML_Training
+//  ContentView.swift
+//  MNIST-CoreML-Training
 //
 //  Created by Jacopo Mangiavacchi on 3/29/20.
 //  Copyright © 2020 Jacopo Mangiavacchi. All rights reserved.
 //
 
-import Foundation
-import CoreML
+import SwiftUI
 
-public class MNIST : ObservableObject {
-    @Published public var batchProvider: MLBatchProvider?
+struct ContentView: View {
+    @ObservedObject var mnist = MNIST()
     
-    public init() {
-        DispatchQueue.global(qos: .userInitiated).async {
-            let provider = self.prepareBatchProvider()
-            DispatchQueue.main.async {
-                self.batchProvider = provider
+    var body: some View {
+        VStack {
+            Text("\(mnist.batchStatus.description) \(mnist.batchProvider?.count ?? 0)")
+            Button(action: {
+                self.mnist.asyncPrepareBatchProvider()
+            }) {
+                Text("Start")
             }
         }
     }
+}
 
-    func prepareBatchProvider() -> MLBatchProvider {
-        func oneHotEncode(_ n: Int) -> [Int] {
-            var encode = Array(repeating: 0, count: 10)
-            encode[n] = 1
-            return encode
-        }
-
-        var featureProviders = [MLFeatureProvider]()
-        
-        errno = 0
-        let trainFilePath = Bundle.main.url(forResource: "mnist_train", withExtension: "csv")!
-        if freopen(trainFilePath.path, "r", stdin) == nil {
-            print("error opening file")
-        }
-        while let line = readLine()?.split(separator: ",") {
-            let imageMultiArr = try! MLMultiArray(shape: [28, 28], dataType: .float32)
-            let outputMultiArr = try! MLMultiArray(shape: [10], dataType: .int32)
-
-            for r in 0..<28 {
-                for c in 0..<28 {
-                    let i = (r*28)+c
-                    imageMultiArr[i] = NSNumber(value: Float(String(line[i + 1]))! / Float(255.0))
-                }
-            }
-
-            let oneHot = oneHotEncode(Int(String(line[0]))!)
-            for i in 0..<10 {
-                outputMultiArr[i] = NSNumber(value: oneHot[i])
-            }
-            
-            let imageValue = MLFeatureValue(multiArray: imageMultiArr)
-            let outputValue = MLFeatureValue(multiArray: outputMultiArr)
-
-            let dataPointFeatures: [String: MLFeatureValue] = ["image": imageValue,
-                                                               "output_true": outputValue]
-            
-            if let provider = try? MLDictionaryFeatureProvider(dictionary: dataPointFeatures) {
-                featureProviders.append(provider)
-            }
-        }
-
-        return MLArrayBatchProvider(array: featureProviders)
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
---
+++ MNIST-CoreML-Training/MNIST.swift
@@ -0,0 +1,96 @@
+//
+//  MNIST.swift
+//  CoreML_Training
+//
+//  Created by Jacopo Mangiavacchi on 3/31/20.
+//  Copyright © 2020 Jacopo Mangiavacchi. All rights reserved.
+//
+
+import Foundation
+import CoreML
+
+public class MNIST : ObservableObject {
+    public enum BatchPreparationStatus {
+        case notPrepared
+        case preparing(count: Int)
+        case ready
+        
+        var description: String {
+            switch self {
+            case .notPrepared:
+                return "notPrepared"
+            case .preparing(let count):
+                return "preparing \(count)"
+            case .ready:
+                return "ready"
+            }
+        }
+    }
+    
+    @Published public var batchProvider: MLBatchProvider?
+    @Published public var batchStatus = BatchPreparationStatus.notPrepared
+    
+    public init() {
+    }
+    
+    func asyncPrepareBatchProvider() {
+        func prepareBatchProvider() -> MLBatchProvider {
+            func oneHotEncode(_ n: Int) -> [Int] {
+                var encode = Array(repeating: 0, count: 10)
+                encode[n] = 1
+                return encode
+            }
+
+            var featureProviders = [MLFeatureProvider]()
+            
+            var count = 0
+            errno = 0
+            let trainFilePath = Bundle.main.url(forResource: "mnist_train", withExtension: "csv")!
+            if freopen(trainFilePath.path, "r", stdin) == nil {
+                print("error opening file")
+            }
+            while let line = readLine()?.split(separator: ",") {
+                count += 1
+                DispatchQueue.main.async {
+                    self.batchStatus = .preparing(count: count)
+                }
+
+                let imageMultiArr = try! MLMultiArray(shape: [28, 28], dataType: .float32)
+                let outputMultiArr = try! MLMultiArray(shape: [10], dataType: .int32)
+
+                for r in 0..<28 {
+                    for c in 0..<28 {
+                        let i = (r*28)+c
+                        imageMultiArr[i] = NSNumber(value: Float(String(line[i + 1]))! / Float(255.0))
+                    }
+                }
+
+                let oneHot = oneHotEncode(Int(String(line[0]))!)
+                for i in 0..<10 {
+                    outputMultiArr[i] = NSNumber(value: oneHot[i])
+                }
+                
+                let imageValue = MLFeatureValue(multiArray: imageMultiArr)
+                let outputValue = MLFeatureValue(multiArray: outputMultiArr)
+
+                let dataPointFeatures: [String: MLFeatureValue] = ["image": imageValue,
+                                                                   "output_true": outputValue]
+                
+                if let provider = try? MLDictionaryFeatureProvider(dictionary: dataPointFeatures) {
+                    featureProviders.append(provider)
+                }
+            }
+
+            return MLArrayBatchProvider(array: featureProviders)
+        }
+        
+        self.batchStatus = .preparing(count: 0)
+        DispatchQueue.global(qos: .userInitiated).async {
+            let provider = prepareBatchProvider()
+            DispatchQueue.main.async {
+                self.batchProvider = provider
+                self.batchStatus = .ready
+            }
+        }
+    }
+}
