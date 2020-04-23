3fa80e31e811667480c941be3d16937d530f2c9c
Jacopo Mangiavacchi <jamangia@microsoft.com>
2020-04-22 18:04:36 -0700

    better split form and draw view

--- MNIST-CoreML-Training/ContentView.swift
+++ MNIST-CoreML-Training/ContentView.swift
@@ -12,6 +12,8 @@ struct ContentView: View {
     @ObservedObject var mnist = MNIST()
     @ObservedObject var drawData = DrawData()
     
+    let splitRatio: CGFloat = 0.2445
+    
     func isDataReady(for status: MNIST.BatchPreparationStatus) -> Bool {
         switch status {
         case .ready: return true
@@ -27,99 +29,102 @@ struct ContentView: View {
     }
 
     var body: some View {
-        VStack {
-            Form {
-                Section(header: Text("Dataset")) {
-                    HStack {
-                        Text("Training: \(self.mnist.trainingBatchStatus.description)")
-                        if self.isDataReady(for: self.mnist.trainingBatchStatus) {
-                            Text(" \(self.mnist.trainingBatchProvider!.count) samples")
+        GeometryReader { geometry in
+            VStack(spacing: 0) {
+                Form {
+                    Section(header: Text("Dataset")) {
+                        HStack {
+                            Text("Training: \(self.mnist.trainingBatchStatus.description)")
+                            if self.isDataReady(for: self.mnist.trainingBatchStatus) {
+                                Text(" \(self.mnist.trainingBatchProvider!.count) samples")
+                            }
+                            Spacer()
+                            Button(action: {
+                                self.mnist.asyncPrepareTrainBatchProvider()
+                            }) {
+                                Text("Start")
+                            }.disabled(self.isDataPreparing(for: self.mnist.trainingBatchStatus))
                         }
-                        Spacer()
-                        Button(action: {
-                            self.mnist.asyncPrepareTrainBatchProvider()
-                        }) {
-                            Text("Start")
-                        }.disabled(self.isDataPreparing(for: self.mnist.trainingBatchStatus))
-                    }
-                    HStack {
-                        Text("Prediction: \(self.mnist.predictionBatchStatus.description)")
-                        if self.isDataReady(for: self.mnist.predictionBatchStatus) {
-                            Text(" \(self.mnist.predictionBatchProvider!.count) samples")
-                        }
-                        Spacer()
-                        Button(action: {
-                            self.mnist.asyncPreparePredictionBatchProvider()
-                        }) {
-                            Text("Start")
-                        }.disabled(self.isDataPreparing(for: self.mnist.predictionBatchStatus))
-                    }
-                }
-                Section(header: Text("Training")) {
-                    Stepper(value: self.$mnist.epoch, in: 1...10, label: { Text("Epoch:  \(self.mnist.epoch)")})
-                    HStack {
-                        Text("Prepare model")
-                        Spacer()
-                        Button(action: {
-                            self.mnist.prepareModel()
-                        }) {
-                            Text("Start")
-                        }.disabled(!self.isDataReady(for: self.mnist.trainingBatchStatus))
-                    }
-                    HStack {
-                        Text("Compile model")
-                        Spacer()
-                        Button(action: {
-                            self.mnist.compileModel()
-                        }) {
-                            Text("Start")
-                        }.disabled(!self.mnist.modelPrepared)
-                    }
-                    HStack {
-                        Text(self.mnist.modelStatus)
-                        Spacer()
-                        Button(action: {
-                            self.mnist.trainModel()
-                        }) {
-                            Text("Start")
-                        }.disabled(!self.mnist.modelCompiled)
-                    }
-                }
-                Section(header: Text("Validation")) {
-                    HStack {
-                        Text("Predict Test data")
-                        Spacer()
-                        Button(action: {
-                            self.mnist.testModel()
-                        }) {
-                            Text("Start")
-                        }.disabled(!self.isDataReady(for: self.mnist.predictionBatchStatus) || !self.mnist.modelTrained)
-                    }
-                    Text(self.mnist.accuracy)
-                }
-                Section(header: Text("Test")) {
-                    HStack {
-                        Button(action: {}) {
-                            Text("Clear")
-                        }.onTapGesture {
-                            self.drawData.lines.removeAll()
-                        }
-                        Spacer()
-                        Text("-")
-                        Spacer()
-                        Button(action: {}) {
-                            Text("Detect")
-                        }.onTapGesture {
-                            let pb = self.drawData.view.getImageData()
-                            print(pb)
+                        HStack {
+                            Text("Prediction: \(self.mnist.predictionBatchStatus.description)")
+                            if self.isDataReady(for: self.mnist.predictionBatchStatus) {
+                                Text(" \(self.mnist.predictionBatchProvider!.count) samples")
+                            }
+                            Spacer()
+                            Button(action: {
+                                self.mnist.asyncPreparePredictionBatchProvider()
+                            }) {
+                                Text("Start")
+                            }.disabled(self.isDataPreparing(for: self.mnist.predictionBatchStatus))
                         }
                     }
-                }
+                    Section(header: Text("Training")) {
+                        Stepper(value: self.$mnist.epoch, in: 1...10, label: { Text("Epoch:  \(self.mnist.epoch)")})
+                        HStack {
+                            Text("Prepare model")
+                            Spacer()
+                            Button(action: {
+                                self.mnist.prepareModel()
+                            }) {
+                                Text("Start")
+                            }.disabled(!self.isDataReady(for: self.mnist.trainingBatchStatus))
+                        }
+                        HStack {
+                            Text("Compile model")
+                            Spacer()
+                            Button(action: {
+                                self.mnist.compileModel()
+                            }) {
+                                Text("Start")
+                            }.disabled(!self.mnist.modelPrepared)
+                        }
+                        HStack {
+                            Text(self.mnist.modelStatus)
+                            Spacer()
+                            Button(action: {
+                                self.mnist.trainModel()
+                            }) {
+                                Text("Start")
+                            }.disabled(!self.mnist.modelCompiled)
+                        }
+                    }
+                    Section(header: Text("Validation")) {
+                        HStack {
+                            Text("Predict Test data")
+                            Spacer()
+                            Button(action: {
+                                self.mnist.testModel()
+                            }) {
+                                Text("Start")
+                            }.disabled(!self.isDataReady(for: self.mnist.predictionBatchStatus) || !self.mnist.modelTrained)
+                        }
+                        Text(self.mnist.accuracy)
+                    }
+                    Section(header: Text("Test")) {
+                        HStack {
+                            Button(action: {}) {
+                                Text("Clear")
+                            }.onTapGesture {
+                                self.drawData.lines.removeAll()
+                            }
+                            Spacer()
+                            Text("-")
+                            Spacer()
+                            Button(action: {}) {
+                                Text("Detect")
+                            }.onTapGesture {
+                                let pb = self.drawData.view.getImageData()
+                                print(pb)
+                            }
+                        }
+                    }
+                }.frame(width: geometry.size.width, height: geometry.size.height - (geometry.size.height * self.splitRatio))
+                
+                Draw()
+                    .environmentObject(self.drawData)
+                    .frame(width: geometry.size.height * self.splitRatio, height: geometry.size.height * self.splitRatio)
+                    .border(Color.blue, width: 1)
             }
-            Draw()
-                .environmentObject(self.drawData)
-                .frame(minWidth: 200, maxWidth: 200, minHeight: 200, maxHeight: 200)
-                .border(Color.blue, width: 1)
         }
     }
 }
